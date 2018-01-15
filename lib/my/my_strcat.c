f160e126d4fe96b709e7ac32ed437208f23f3a09
Arthur Dassier <arthur.dassier@epitech.eu>
2018-01-15 18:36:35 +0100

    Nor more need of /bin/

--- lib/my/my_strcat.c
+++ lib/my/my_strcat.c
@@ -5,17 +5,22 @@
 ** strcat
 */
 
+#include <stdlib.h>
+
 char *my_strcat(char *dest, char const *src)
 {
 	int	i = 0;
 	int	j = 0;
+	char	*tmp = malloc(55);
+
 	while (dest[i] != '\0') {
-		i++;
+		tmp[i] = dest[i];
+		++i;
 	}
 	while (src[j] != '\0') {
-		dest[i] = src[j];
+		tmp[i] = src[j];
 		i++;
 		j++;
 	}
-	return (dest);
+	return (tmp);
 }
