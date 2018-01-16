f1bb07f46cb2998334b5f581d7b0c019641f5030
Arthur Dassier <arthur.dassier@epitech.eu>
2018-01-16 11:13:48 +0100

    No more '/bin/' string

--- srcs/brain.c
+++ srcs/brain.c
@@ -12,14 +12,12 @@ int main()
 	char	*str = malloc(55);
 	int	lol = 0;
 	char	*buf[] = {"NULL", NULL};
-	char	bin[5] = "/bin/";
 	pid_t	pid;
 
 	while (1) {
 		my_putstr("ZSH_2000: ");
 		lol = read(0, str, 50);
 		str[lol - 1] = '\0';
-		str = my_strcat(bin, str);
 		if ((pid = fork()) == 0)
 			execve(str, buf, NULL);
 		else
