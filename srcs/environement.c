d986227efbf0c92e59bb6f37b8daa2d10ce5d127
Arthur Dassier <arthur.dassier@epitech.eu>
2018-01-18 18:23:45 +0100

    Setenv as long as it doesn't exist

--- srcs/environement.c
+++ srcs/environement.c
@@ -7,10 +7,30 @@
 
 #include "my.h"
 
+char *my_set(char *str)
+{
+	int	i = 0;
+	int	j = 0;
+	char	*tmp = malloc(100);
+
+	while (str[i] != ' ') {
+		++i;
+	}
+	++i;
+	while (str[i] != '\0') {
+		tmp[j] = str[i];
+		if (tmp[j] == ' ')
+			tmp[j] = '=';
+		++j;
+		++i;
+	}
+	return (tmp);
+}
+
 int check_existing(char *checking, char *str)
 {
 	int	i = 0;
-	int	j = 6;
+	int	j = 7;
 
 	while (checking[i] != '=') {
 		if (checking[i] != str[j])
@@ -39,14 +59,16 @@ void check_envvar(char *str, list_path *list_p)
 		temp = temp->next;
 	}
 	if (i == 0) {
+		str = my_set(str);
 		insert_end(&list_p, str);
+		print_list(list_p);
 	}
 }
 
 void modif_env(char *str, list_path *list_p)
 {
-	if (str[6] == '\0')
-		print_list(list_p);
-	if (str[6] == ' ')
+	if (str[6])
 		check_envvar(str, list_p);
+	else
+		print_list(list_p);
 }
