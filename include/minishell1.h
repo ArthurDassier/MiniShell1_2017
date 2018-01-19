e2c3440f3ffeb340375e7586e49f68db547f6e7c
Arthur Dassier <arthur.dassier@epitech.eu>
2018-01-19 11:29:21 +0100

    missing ';' in minishell.h

--- include/minishell1.h
+++ include/minishell1.h
@@ -18,18 +18,22 @@ struct list_path
 	list_path	*next;
 };
 
+int check_env(char *str);
+int check_setenv(char *str);
+void check_arg(char *str, list_path *list_p, char **env);
+void bin_tests(char *str, list_path *list_p, char **env);
+char *lead_path(char *str, char *path, int *j);
+char **set_buffer(char *tmp);
+char **multi_arg(char *tmp, char **buffer, int i);
 list_path *init_cl(char *path);
 void insert_begin(list_path **first_elem, char *path);
 void insert_end(list_path **first_elem, char *path);
 void print_list(list_path *liste);
 void error_handler(char *str);
-void bin_tests(char *str, list_path *list_p, char **env);
 void wait_com(char *str, char **env, list_path *list_p);
-char *lead_path(char *str, char *path, int *j);
 list_path *init_chain_path(char **env);
-int get_path(char **env);
 int fct_while(char *str, char **env);
-void check_arg(char *str, list_path *list_p, char **env);
+int get_path(char **env);
 void modif_env(char *str, list_path *list_p);
 
 #endif
